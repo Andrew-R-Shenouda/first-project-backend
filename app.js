b6949728532fec5c37f43e5f16514ab46aaa4e5d
Andrew-R-Shenouda <ashenouda50@gmail.com>
2023-12-13 23:47:55 -0500

    add functionality to support users

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ import supportedCurrencyRoutes from "./supported-currencies/routes.js";
 import http from "http";
 import WebSocket, { WebSocketServer } from "ws";
 import CreateCoinbaseWebSocketConnection from "./websocket-connection-market-data/coinbase-websocket-connection.js";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
 
+mongoose.connect("mongodb://127.0.0.1:27017/crypto");
 const app = express();
 app.use(cors());
 app.use(express.json());
 userCurrencyRoutes(app);
 supportedCurrencyRoutes(app);
+UserRoutes(app);
 const httpserver = http.createServer(app);
 const wss = new WebSocketServer({
   port: 4040,
 });
-const coinbaseConnection = CreateCoinbaseWebSocketConnection(wss); 
+const coinbaseConnection = CreateCoinbaseWebSocketConnection(wss);
 export default coinbaseConnection;
 httpserver.listen(4000);
